b7185175aaa91f9d00767ef69ca8703080616f03
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-05-03 10:41:37 +0700

    test all

--- app/controllers/add_stage_item.controller.js
+++ app/controllers/add_stage_item.controller.js
@@ -1,6 +1,7 @@
 const connection = require("../commons/connect")
 
 const add_stage_item= async (req, res)=> {
+    // console.log(req.body)
     try {
         const {device, mode, startPoint, endPoint, timeStart, timeEnd, stage_id, state, date }= req.body
         connection.query("INSERT INTO stage_item(device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id, date) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", [device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id, date], (err, data)=> {
